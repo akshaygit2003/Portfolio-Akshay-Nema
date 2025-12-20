3b57f91978c9e2c2045ddf7fedb3a08b5b8f5965
akshay-nema-omniful <akshay.nema@omniful.ai>
2025-12-20 20:42:58 +0530

    added comment for testing

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-
-/* Componet */
+/* Components */
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import Main from '../../components/Main';
